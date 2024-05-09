f63b25837ceb4fe6a7e8127cdaedcd74cf84ebf0
rahulsharma0403 <rahul.sharma0403@gmail.com>
2024-05-09 08:01:13 -0400

    Add files via upload

---
+++ src/test/features/login.feature
@@ -0,0 +1,20 @@
+Feature: User Authentication tests
+
+Background:  // if we want to execute something first
+   Given User navigates to the application
+   When User Click on the login link
+
+
+Scenario: Login should be success
+    Given User enter the username as "<rahul_sharma>"
+    When user enter the password as "<Stagingdemo@0403>"
+    When User click on the login button
+    Then Login should be success
+
+
+Scenario: Login should not be success
+  Given User enter the username as "Rahul"
+  When user enter the password as "Sharma"
+  When User click on the login button
+  But Login should fail
+ 
---
+++ src/test/steps/login.ts
@@ -0,0 +1,41 @@
+import { Given , When, Then} from "@cucumber/cucumber"
+import {chromium, Page, Browser, expect} from "@playwright/test";
+
+let browser: Browser;
+let page: Page;
+
+Given('User navigates to the application', async function(){
+     browser = await chromium.launch ({headless:false});
+     page = await browser.newPage();
+     await page.goto("https://staging-demo.yuja.com/");
+});
+
+When ('User Click on the login link',async function(){
+   await page.locator("//button[@id='loginBtn']").click();
+});
+
+Given ('User enter the username as {string}',async function(username){
+   await page.locator("//input[@id='loginuserid']").type(username);
+});
+
+When ('user enter the password as {string}',async function(password){
+  await page.locator("//input[@id='password']").type(password);
+});
+
+When ('User click on the login button',async function(){
+  await page.locator("//button[@id='loginButton']").click();
+});
+
+Then('Login should be success', async function(){
+  const text =  await page.locator("//span[@id='topBarTabName3']']").textContent() ;
+  console.log("Manage Media" + text);
+  await browser.close();
+});
+
+When('Login should fail',async function(){
+  const text =  page.locator("div[id='loginMessage'] div div span").textContent();
+  //await expect(text).toBeVisible();
+  //await expect(page.getByText('Welcome')).toBeVisible();
+
+  await browser.close();
+});
