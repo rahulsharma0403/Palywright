4f06f780a2c09923e9fc6eb1e949cd5dfb2087e9
rahulsharma0403 <rahul.sharma0403@gmail.com>
2024-05-14 21:59:45 -0400

    Update login.ts

--- src/test/steps/login.ts
+++ src/test/steps/login.ts
@@ -28,8 +28,53 @@ When('User click on the login button', async function () {
 });
 
 Then('Login should be success', async function () {
-  await expect (page.locator("#topBarTabName3")).toHaveText('Manage Media');
-  console.log("Login in is a success")
+  
+  expect (page.locator("#topBarTabName3")).toBeVisible({timeout: 10000});
+  await expect (page.locator("#topBarTabName3")).toContainText('Manage Media');
+  console.log("Login in is a success");
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  var manage_media = page.locator("#topBarTabName3");
+  await manage_media.click();
+  var media_library = page.locator('#firstPartText');
+  await expect (media_library).toContainText('Media Library');
+  var create_recording = page.locator('#topBarTabName2');
+  await expect (create_recording).toContainText('Create Recording')
+  var video_conference = page.locator('#topBarTabName1');
+  await expect (video_conference).toContainText('Video Conference');
+  var new_folder = page.locator("div[id='btnNewFolder'] span[class='file-explore-icon']");
+  await expect (new_folder).toContainText('NEW FOLDER');
+  var new_playlist = page.locator ("div[id='btnNewFolder'] span[class='file-explore-icon']");
+  await expect (new_playlist).toContainText('NEW PLAYLIST');
+  var upload = page.locator("div[id='btnUploadMedia'] span[class='file-explore-icon']");
+  await expect(upload).toContainText('UPLOAD');
+  var more_actions = page.locator("span[class='btn btn-sm btn-link'] span[class='file-explore-icon']");
+  await expect (more_actions).toContainText('MORE ACTIONS');
+  var sort = page.locator("button[title='Sort']");
+  await expect(sort).toContainText('SORT');
+  var my_media = page.locator("div[title='My Media'] span");
+  await expect(my_media).toContainText('My Media');
+  var shared_with_me = page.locator("div[title='Shared With Me'] span");
+  await expect(shared_with_me).toContainText('Shared With Me');
+  var favorites = page.locator("div[title='Favorites'] span");
+  await expect(favorites).toContainText('Favorites');
+  var shared_with_others = page.locator("div[title='Shared With Others'] span");
+  await expect(shared_with_others).toContainText('Shared With Others');
+  var playlist = page.locator("div[title='Playlists'] span");
+  await expect(playlist).toContainText('Playlists');
+  var shared = page.locator("#Media-library-sidebar-shared");
+  await expect(shared).toContainText('SHARED');
+  var enterprisetube = page.locator("div[title='EnterpriseTube'] span");
+  await expect(enterprisetube).toContainText('EnterpriseTube');
+  var internal_library = page.locator("div[title='Internal Library'] span");
+  await expect(internal_library).toContainText('Internal Library');
+  var course_channels = page.locator("div[title='Course Channels'] span");
+  await expect(course_channels).toContainText('Course Channels');
+  var user_collections = page.locator("div[title='User Collections'] span");
+  await expect(user_collections).toContainText('User Collections');
+  var archive = page.locator("#Media-library-sidebar-archive");
+  await expect(archive).toContainText('ARCHIVE');
+  var my_archive = page.locator("#Media-library-sidebar-archive");
+  await expect(my_archive).toContainText('ARCHIVE');
   await browser.close();
 });
 
